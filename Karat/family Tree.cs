19f27ee568dc2e8d5b79940165868daec15d06ce
murlizeus <47632457+murlizeus@users.noreply.github.com>
2022-11-01 12:49:47 +0530

    Create family Tree.cs

---
+++ Karat/family Tree.cs
@@ -0,0 +1,11 @@
+/*
+Prompt: Given an array of arrays, where each array contains two numbers - the first number being a parent and the second 
+number being a child of a tree - return an array of arrays where the 0ith element is an array of all nodes with no parents (roots), 
+and the 1st element is an array of all nodes with 3 parents.
+signature getRootAndNodesWithThreeParents(arrayOfRelationships)
+
+Technical Question 2
+Prompt: Given an array of arrays, where each array contains two numbers - the first number being a parent and the second number 
+being a child of a tree -, and two nodes - node1 and node2 - return an array of all common anscestors.
+
+*/
